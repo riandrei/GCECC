f813f362eb0ccd79a3fde3cd5223939eea760745
Riandrei Casañas <riandrei.casanas@gmail.com>
2023-04-03 14:58:42 +0800

    Removed some unnecessary code

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,3 @@
-import { createStore, applyMiddleware, compose } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
@@ -6,10 +5,6 @@ import rootReducer from './reducers';
 const initialState = {};
 
 const middleWare = [thunk];
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleWare)));
 
 const store = configureStore({
   reducer: rootReducer,
