6e0086a306c7a95f38dceab0eb4abb9345516aaa
Riandrei Casañas <riandrei.casanas@gmail.com>
2023-05-29 20:32:25 +0800

    Made the orderDetail component dynamic

--- client/src/components/OrderDetail.jsx
+++ client/src/components/OrderDetail.jsx
@@ -1,11 +1,44 @@
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation, useParams } from 'react-router-dom';
 
 import '../css/orderdetail.css';
 
 import hamburgerMenu from '../assets/hamburger-menu.png';
+import noImg from '../assets/no-image.jpg';
 
 const OrderDetail = () => {
+  const { orderId } = useParams();
+  const location = useLocation();
+
+  const orders = useSelector((state) => state.order.orders);
+  const itemList = useSelector((state) => state.item.items);
+  let clickedOrder, orderItems;
+
+  const [name, setName] = useState(``);
+  const [department, setDepartment] = useState(``);
+  const [domain, setDomain] = useState(``);
+  const [bill, setBill] = useState(``);
+  const [items, setItems] = useState([]);
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+  useEffect(() => {
+    clickedOrder = orders.find((order) => order._id === orderId);
+    orderItems = clickedOrder.items.map((item) => item);
+
+    orderItems.forEach((item) => {
+      const foundItem = itemList.find((itemEntry) => itemEntry._id === item.itemId);
+      item.imgUrl = foundItem.img_url[0];
+      item.price = foundItem.price;
+      item.label = foundItem.label;
+    });
+
+    setName(`${clickedOrder?.userName}`);
+    setDepartment(`${clickedOrder?.userDepartment} Department`);
+    setDomain(`${clickedOrder?.userDomain}`);
+    setBill(`${clickedOrder?.bill}`);
+    setItems(orderItems);
+  }, [orders]);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -58,42 +91,41 @@ const OrderDetail = () => {
         </div>
       </div>
       <div className="order-detail-contents">
-        <div className="order-items">
-          <img
-            src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2017/11/Tshirt-design.jpg?auto=format&q=60&w=2060&h=1236&fit=crop&crop=faces"
-            alt=""
-          />
-          <div className="items-detail">
-            <p>Gordon College Male Polo</p>
-            <p>Large</p>
-            <p>x1</p>
-            <p>₱42,069</p>
+        {items.map((orderItem) => (
+          <div className="order-items">
+            <img src={orderItem.imgUrl || noImg} alt="item-image" />
+            <div className="items-detail">
+              <p>{orderItem.label}</p>
+              <p>{`${orderItem.size.charAt(0).toUpperCase()}${orderItem.size.slice(1, orderItem.size.length)}`}</p>
+              <p>{`x${orderItem.quantity}`}</p>
+              <p>{orderItem.price}</p>
+            </div>
           </div>
-        </div>
+        ))}
         <div className="user-details">
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
             </svg>
-            <p>Riandrei Kenneth Casanas</p>
+            <p>{name}</p>
           </div>
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
               <path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z" />
             </svg>
-            <p>CCS Department</p>
+            <p>{department}</p>
           </div>
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
             </svg>
-            <p>202110203@gordoncollege.edu.ph</p>
+            <p>{domain}</p>
           </div>
           <div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
               <path d="M64 32C46.3 32 32 46.3 32 64v64c-17.7 0-32 14.3-32 32s14.3 32 32 32l0 32c-17.7 0-32 14.3-32 32s14.3 32 32 32l0 64v96c0 17.7 14.3 32 32 32s32-14.3 32-32V384h80c68.4 0 127.7-39 156.8-96H352c17.7 0 32-14.3 32-32s-14.3-32-32-32h-.7c.5-5.3 .7-10.6 .7-16s-.2-10.7-.7-16h.7c17.7 0 32-14.3 32-32s-14.3-32-32-32H332.8C303.7 71 244.4 32 176 32H64zm190.4 96H96V96h80c30.5 0 58.2 12.2 78.4 32zM96 192H286.9c.7 5.2 1.1 10.6 1.1 16s-.4 10.8-1.1 16H96V192zm158.4 96c-20.2 19.8-47.9 32-78.4 32H96V288H254.4z" />
             </svg>
-            <p>50000</p>
+            <p>{bill}</p>
           </div>
         </div>
       </div>
