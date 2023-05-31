ed125020cb142d2d150eb1f9e9e839b62b3ce467
Riandrei Casañas <riandrei.casanas@gmail.com>
2023-05-31 11:29:00 +0800

    Changed the style of the store items

--- client/src/components/Store.jsx
+++ client/src/components/Store.jsx
@@ -38,9 +38,9 @@ const Store = () => {
 
   return (
     <div className="store-main-container" onClick={toggleNav}>
-      <Header />
       {categories.map((category) => (
         <div key={category._id} className="store-category">
+          <Header />
           <div className="store-category-title">
             <h1>{category.category_name}</h1>
           </div>
@@ -50,31 +50,10 @@ const Store = () => {
               (item) =>
                 category._id == item.category && (
                   <Link key={item._id} to={`/user/store/${item._id}`} className="product-contents">
-                    <div className="box-up">
-                      {/* PULL FROM THE DATABASE */}
-                      <img src={item.img_url[0] ? item.img_url[0] : noImg} height="300px" className="img" alt="" />
-                      <div className="img-info">
-                        <div className="info-inner">
-                          <span className="p-name">{item.label}</span>
-                          <span className="p-company">Gordon College</span>
-                        </div>
-                        <div className="a-size">
-                          Sizes : <span className="size">S , M , L , XL</span>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="box-down">
-                      <div className="h-bg">
-                        <div className="h-bg-inner"></div>
-                      </div>
-
-                      <p className="cart" href="#">
-                        <span className="price">{`₱${item.price}`}</span>
-                        <span className="add-to-cart">
-                          <span className="txt">Add in cart</span>
-                        </span>
-                      </p>
+                    <div className="uni-card male reflect" style={{ backgroundImage: `url(${item.img_url})` }}>
+                      <h1>Gordon College</h1>
+                      <img src={item.img_url} className="backdrop-image" alt="" />
+                      <p>{item.label}</p>
                     </div>
                   </Link>
                 )
